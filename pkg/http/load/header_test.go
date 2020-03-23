5c2e65101148571f8d21330dc151af72a2cdb0b6
Michal Gosek <inbox@michalgosek.com>
2020-03-23 13:09:10 +0100

    Added Content-Types check.

--- pkg/http/load/header_test.go
+++ pkg/http/load/header_test.go
@@ -15,6 +15,7 @@ import (
 type testContent struct {
 	payloadContent   *strings.Reader
 	validationResult PayloadValidationError
+	contentType      string
 }
 
 func fakeHandler(w http.ResponseWriter, r *http.Request) PayloadValidationError {
@@ -26,16 +27,17 @@ func fakeHandler(w http.ResponseWriter, r *http.Request) PayloadValidationError 
 var _ = Describe("When calling PostPayloadCheck", func() {
 
 	var data []testContent
-
 	BeforeEach(func() {
 		data = []testContent{
 			{
 				strings.NewReader(`{"url": "https://httpbin.org/range/15","interval":60}`),
 				PayloadValidationError{Status: http.StatusAccepted, Msg: fmt.Sprintln("Payload check validation was succed.")},
+				"application/json",
 			},
 			{
 				strings.NewReader(`{"url": "https://httpbin.org/delay/15","interval":110}`),
 				PayloadValidationError{Status: http.StatusAccepted, Msg: fmt.Sprintln("Payload check validation was succed.")},
+				"application/json",
 			},
 		}
 	})
@@ -58,54 +60,77 @@ var _ = Describe("When calling PostPayloadCheck", func() {
 				{
 					strings.NewReader(`{"url": "https://httpbin.org/range/40"}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Missing interval field in JSON payload.")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{"url": "https://httpbin.org/range/40", "interval":10}Woops!'`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Request body must cotain only single JSON object.")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{"url": 1234, "interval": ""}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln(`Request body contains an invalid value for the "url" field (at position 12).`)},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{"url": "http://httpbin.org/range/15","interval":"abc"}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln(`Request body contains an invalid value for the "interval" field (at position 54).`)},
+					"application/json",
 				},
 				{
 					strings.NewReader(``),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Request body must not be empty.")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Missing url and interval fields in JSON payload.")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{"interval":10}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Missing url field in JSON payload.")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{interval:""}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Request body contains badly-formed JSON (at position 2).")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{interval:}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Request body contains badly-formed JSON (at position 2).")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{"url":"abc"}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Missing interval field in JSON payload.")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{url:""}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Request body contains badly-formed JSON (at position 2).")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{url:}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln("Request body contains badly-formed JSON (at position 2).")},
+					"application/json",
 				},
 				{
 					strings.NewReader(`{"url": "https://httpbin.org/range/15","interval":60, "zonk": "Hello!"}`),
 					PayloadValidationError{Status: http.StatusBadRequest, Msg: fmt.Sprintln(`Request body contains unknown field "zonk".`)},
+					"application/json",
+				},
+				{ // Invalid Content-Types:
+					strings.NewReader(`{"url": "https://httpbin.org/range/15","interval":60, "zonk": "Hello!"}`),
+					PayloadValidationError{Status: http.StatusUnsupportedMediaType, Msg: fmt.Sprintln("Invalid or lack of Content-Type.")},
+					"text/html",
+				},
+				{
+					strings.NewReader(`{"url": "https://httpbin.org/range/15","interval":60, "zonk": "Hello!"}`),
+					PayloadValidationError{Status: http.StatusUnsupportedMediaType, Msg: fmt.Sprintln("Invalid or lack of Content-Type.")},
+					"application/x-www-form-urlencoded",
 				},
 			}
 		})
@@ -113,6 +138,7 @@ var _ = Describe("When calling PostPayloadCheck", func() {
 		It("Should return correct httpStatus and validation msg.", func() {
 			for _, el := range data {
 				r := httptest.NewRequest(http.MethodPost, "/", el.payloadContent)
+				r.Header.Set("Content-Type", el.contentType)
 				w := httptest.NewRecorder()
 				result := fakeHandler(w, r)
 				Expect(result.Status).To(Equal(el.validationResult.Status))
@@ -121,5 +147,3 @@ var _ = Describe("When calling PostPayloadCheck", func() {
 		})
 	})
 })
-
-// TO-DO: Add test cases with invalid content types!
